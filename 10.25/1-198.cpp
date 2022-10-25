26eccde9c2bc9898ae335ad7243c8dc352694806
HangyuZ <69741140+HangyuZ@users.noreply.github.com>
2022-10-25 10:36:04 +0800

    80

--- 10.25/1-198.cpp
+++ 10.25/1-198.cpp
@@ -1,6 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+int a[200005];bool p[200005]={false};
 int main(){
-    
+    int n;int x;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        scanf("%d",&x);
+        if(!p[x]) {a[i]=x;p[x]=true;}
+    }
+    for(int i=1;i<=n;i++){
+        if(a[i]!=0) printf("%d ",a[i]);
+    }
     return 0;
 }
