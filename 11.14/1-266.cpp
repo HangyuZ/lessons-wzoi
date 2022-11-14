df2a63c796766790804b5841527200eaac899fb0
HangyuZ <69741140+HangyuZ@users.noreply.github.com>
2022-11-14 09:15:27 +0800

    don't copy

---
+++ 11.14/1-266.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct person{
+    int no;
+	int score;
+}p[100005];
+int paixu(person a,person b){
+    if(a.score!=b.score) return a.score>b.score;
+    else return a.no<b.no;
+}
+int main(){
+    int m,n;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){cin>>p[i].no;cin>>p[i].score;}
+    m*=1.5;
+    sort(p+1,p+1+n,paixu);
+    int i=0;
+    while(i<=m-1){
+        i++;
+        cout<<p[i].no<<' '<<p[i].score<<endl;
+    }
+    while
+    return 0;
+}//||p[i].score==p[i+1].score
