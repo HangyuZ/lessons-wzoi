068bed212cb5ac3646b0b37f4e348d209fcd0ea7
HangyuZ <69741140+HangyuZ@users.noreply.github.com>
2022-11-16 00:03:18 +0000

    AC Done

--- 11.15/1-559.cpp
+++ 11.15/1-559.cpp
@@ -1,17 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct card{
-    int data[15];
-    int num;
-    int rank;
-}a[100005]
-int main(){
-    int m,n;
-    cin>>n>>m;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++)
-            cin<<a[i].data[j];
-        a[i].num=i;}
-    sort(a+1,a+1+n,paixu);
-    
+struct data{
+	int x[11],num;
+};
+data a[50005];
+int n,m;
+bool cmp(data a,data b){
+	for (int i=1;i<=m;i++)
+	if (a.x[i]>b.x[i]) return true;
+	else if (a.x[i]<b.x[i]) return false;
+    return a.num<b.num;
+}
+int main() {
+	scanf ("%d%d",&n,&m);
+	for (int i=1;i<=n;i++){
+		a[i].num =i;
+		for (int j=1;j<=m;j++) scanf("%d",&a[i].x[j]);
+	}
+	sort(a+1,a+n+1,cmp);
+	for(int i =1;i<=n;i++)
+	printf("%d ",a[i].num);
+	
+	return 0;
 }
