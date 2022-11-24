5429899cacec073494e0bca3f167d8441617e897
HangyuZ <69741140+HangyuZ@users.noreply.github.com>
2022-11-24 02:00:03 +0000

    放弃挣扎

---
+++ 11.23/1-1737.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void p(int a,int x){
+    if(x=a/2+1){
+        for(int i=1;i<=a/2;i++) cout<<' ';
+        cout<<'X';
+    }
+    else for(int i=1;i<=)
+}
+int main(){
+
+}
