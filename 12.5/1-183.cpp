0a09d436be1fe614b644db1cd276b4f29a4a981c
HangyuZ <69741140+HangyuZ@users.noreply.github.com>
2022-12-05 01:15:08 +0000

    undone

---
+++ 12.5/1-183.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[105][105];bool b[105][105];
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=m;j++)
+            {scanf("%d",%a[i][j]);b[i][j]=true;}
+    int i=1,j=1;
+    while()
+}
