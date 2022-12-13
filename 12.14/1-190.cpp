9d26e9505017e5bfd0557af234c5b390d6d4e1f9
HangyuZ <69741140+HangyuZ@users.noreply.github.com>
2022-12-13 23:47:35 +0000

    AC Done

---
+++ 12.14/1-190.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s,s2="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    getline(cin,s);
+    int i;
+    for(i=0;i<s.length();i+=3){
+        int x=s[i],y=s[i+1],z=s[i+2];
+        int a=x/4;
+        int b=x%4*16+y/16;
+        int c=y%16*4+z/64;
+        int d=z%64;
+        cout<<s2[a]<<s2[b]<<s2[c]<<s2[d];
+    }
+    return 0;
+}
