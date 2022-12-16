75f2a309407a254aaa8d2ecbca8ea7ab54f8d8e1
HangyuZ <69741140+HangyuZ@users.noreply.github.com>
2022-12-16 00:57:50 +0000

    90%

--- 12.14/1-154.cpp
+++ 12.14/1-154.cpp
@@ -3,15 +3,16 @@ using namespace std;
 int main(){
     string s1;
     cin>>s1;string s2=s1;
+    s2.erase(12,1);
     s1.erase(1,1);s1.erase(4,1);s1.erase(9,1);
-    cout<<s1;
     int ans=0;
     for(int i=0;i<=8;i++){
         ans+=(s1[i]-'0')*(i+1);
     }
     int mod=ans%11;
-    if(s1[9]=mod||(s1[9]=='X'&&mod==10))
+    if(s1[9]==mod||(s1[9]=='X'&&mod==10))
         cout<<"Right";
-    
+    else if(mod!=10) cout<<s2<<mod;
+    else if(mod==10) cout<<s2<<'X';
     return 0;
 }
